ea0a6821736d166e0b13ac8faab9fa79692045e6
apthskyi <apthskyi@126.com>
2014-02-27 23:21:23 +0800

    First

---
+++ WPFTest/WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human human = (Human)this.FindResource("human");
+            MessageBox.Show(human.Child.Name);
+        }
+    }
+
+    public class StringToHumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Human human = new Human();
+                human.Name = value as string;
+                return human;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }  
+}
