2428cbba58a1e2d476bc793812fe5a97dccf41dd
apthskyi <apthskyi@126.com>
2014-03-06 17:46:31 +0800

    Property

---
+++ WPFTest/WPFTest/DependencyObjectTest.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for DependencyObjectTest.xaml
+    /// </summary>
+    public partial class DependencyObjectTest : UserControl
+    {
+        public DependencyObjectTest()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Student stu = new Student();
+            stu.SetValue(Student.NameProperty, textBox1.Text);
+            textBox2.SetValue(TextBox.TextProperty, stu.GetValue(Student.NameProperty));
+        }  
+    }
+    public class Student : DependencyObject
+    {
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Student));
+    }
+}
