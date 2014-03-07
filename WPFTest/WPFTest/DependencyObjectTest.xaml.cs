9c2e928d83892ebbaac577a6e101e936ec284232
apthskyi <apthskyi@126.com>
2014-03-07 18:05:20 +0800

    Property Finished

--- WPFTest/WPFTest/DependencyObjectTest.xaml.cs
+++ WPFTest/WPFTest/DependencyObjectTest.xaml.cs
@@ -20,19 +20,40 @@ namespace WPFTest
     /// </summary>
     public partial class DependencyObjectTest : UserControl
     {
+        Student stu;
         public DependencyObjectTest()
         {
             InitializeComponent();
+            stu = new Student();
+            Binding bind=new Binding();
+            bind.Path = new PropertyPath("Text");
+            bind.Source=this.textBox1;
+            bind.Mode = BindingMode.TwoWay;
+
+            stu.SetBinding(Student.NameProperty, bind);
+            textBox2.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = stu });
+            //Binding bind = new Binding("Text") { Source = textBox1 };
+            //BindingOperations.SetBinding(stu, Student.NameProperty, bind); 
         }
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            Student stu = new Student();
-            stu.SetValue(Student.NameProperty, textBox1.Text);
+            //Student stu = new Student();
+            //stu.SetValue(Student.NameProperty, textBox1.Text);
             textBox2.SetValue(TextBox.TextProperty, stu.GetValue(Student.NameProperty));
         }  
     }
     public class Student : DependencyObject
     {
+        public string Name
+        { 
+            set { SetValue(NameProperty, value); } 
+            get { return (string)GetValue(NameProperty); } 
+        }
+        
         public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Student));
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase bind)
+        {
+            return BindingOperations.SetBinding(this, dp, bind);
+        }  
     }
 }
---
+++ WPFTest/WPFTest/Property.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for Property.xaml
+    /// </summary>
+    public partial class Property : Window
+    {
+        public Property()
+        {
+            InitializeComponent();
+            Human11 human = new Human11();
+            School.SetGrade(human, 15);
+            int grade = School.GetGrade(human);
+            MessageBox.Show(grade.ToString());  
+        }
+    }
+    public class School : DependencyObject
+    {
+
+        public static int GetGrade(DependencyObject obj)
+        {
+            return (int)obj.GetValue(GradeProperty);
+        }
+
+        public static void SetGrade(DependencyObject obj, int value)
+        {
+            obj.SetValue(GradeProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Grade.  This enables animation, styling, binding, etc...  
+        public static readonly DependencyProperty GradeProperty =
+            DependencyProperty.RegisterAttached("Grade", typeof(int), typeof(School), new UIPropertyMetadata(0));
+
+    }
+    public class Human11 : DependencyObject
+    {
+
+    }  
+}
